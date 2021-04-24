558152f521219b23705a487e5c7b5ec2239946b9
merr1 <jabishvili31@gmail.com>
2021-04-24 23:56:54 +0400

    Add data from api to localStorage

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -13,14 +13,54 @@ const Quiz = () => {
   const [answer, setAnswer] = useState(null);
   const [qurrentId, setQurrentID] = useState(0);
   const [score, setScore] = useState(0);
+
+  const getData = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+  };
+
+  const saveLocal = async () => {
+    const data = await getData();
+    const now = new Date();
+    localStorage.setItem(
+      "data",
+      JSON.stringify({
+        value: data,
+        expiry: now.getTime() + 60000,
+      })
+    );
+  };
+  const getLocal = () => {
+    const local = localStorage.getItem("data");
+    if (!local) {
+      return null;
+    }
+    const data = JSON.parse(local);
+    const now = new Date();
+    if (now.getTime() > data.expiry) {
+      localStorage.removeItem("data");
+      return null;
+    }
+    return data.value;
+  };
+  const myQuestions = async () => {
+    if (getLocal() === null) {
+      await saveLocal();
+      console.log("movida");
+      setQuestion(getLocal("data").questions);
+      setAnswer(getLocal("data").answers);
+    } else {
+      setQuestion(getLocal("data").questions);
+      setAnswer(getLocal("data").answers);
+      console.log("iyo");
+    }
+  };
+
   useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        setQuestion(data.questions);
-        setAnswer(data.answers);
-      });
+    myQuestions();
   }, []);
+
   const next = () => {
     setQurrentID(qurrentId + 1);
   };
